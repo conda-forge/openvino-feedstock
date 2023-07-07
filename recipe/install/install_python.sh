ed01b2cbb0912821b331f2d0074deac8e3778db0
Ilya Lavrenov <ilya.lavrenov@intel.com>
2023-07-07 16:42:59 +0400

    Build only python

---
+++ recipe/install/install_python.sh
@@ -0,0 +1,11 @@
+# !/usr/bin/env bash
+
+export OPENVINO_BINARY_DIR="$SRC_DIR/build"
+export PY_PACKAGES_DIR="lib/python$PY_VER/site-packages"
+export WHEEL_VERSION="$PKG_VERSION"
+export PYTHON_EXTENSIONS_ONLY="1"
+export SKIP_RPATH="1"
+
+$PYTHON -m pip install --no-deps --ignore-installed -v "$SRC_DIR/src/bindings/python/wheel"
+
+# TODO: do we need to replace ocv.exe with ocv.bat ?
