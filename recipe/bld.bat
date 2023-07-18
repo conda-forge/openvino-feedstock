b1e05234061eed4b29d28d3cc2e21a237c379f5e
Anokhov <artyom.anokhov@intel.com>
2023-07-18 15:48:37 +0200

    Directly specified nvidia_plugin only as extra modules.
    Moved cudatoolkit to build section

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,29 +3,28 @@ setlocal enabledelayedexpansion
 
 mkdir -p build
 
-cmake                                                                        ^
-    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"                                ^
-    -DCMAKE_BUILD_TYPE=Release                                               ^
-    -DENABLE_INTEL_GNA=OFF                                                   ^
-    -DENABLE_SYSTEM_TBB=ON                                                   ^
-    -DENABLE_SYSTEM_PUGIXML=ON                                               ^
-    -DENABLE_SYSTEM_OPENCL=ON                                                ^
-    -DENABLE_SYSTEM_PROTOBUF=ON                                              ^
-    -DENABLE_SYSTEM_SNAPPY=ON                                                ^
-    -DOPENVINO_EXTRA_MODULES="%SRC_DIR%/openvino_contrib/modules"            ^
-    -DBUILD_nvidia_plugin=ON                                                 ^
-    -DBUILD_java_api=OFF                                                     ^
-    -DENABLE_COMPILE_TOOL=OFF                                                ^
-    -DENABLE_PYTHON=OFF                                                      ^
-    -DENABLE_CPPLINT=OFF                                                     ^
-    -DENABLE_CLANG_FORMAT=OFF                                                ^
-    -DENABLE_NCC_STYLE=OFF                                                   ^
-    -DENABLE_TEMPLATE=OFF                                                    ^
-    -DENABLE_SAMPLES=OFF                                                     ^
-    -DENABLE_DATA=OFF                                                        ^
-    -DCPACK_GENERATOR=CONDA-FORGE                                            ^
-    -G Ninja                                                                 ^
-    -S "%SRC_DIR%"                                                           ^
+cmake                                                                               ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"                                       ^
+    -DCMAKE_BUILD_TYPE=Release                                                      ^
+    -DENABLE_INTEL_GNA=OFF                                                          ^
+    -DENABLE_SYSTEM_TBB=ON                                                          ^
+    -DENABLE_SYSTEM_PUGIXML=ON                                                      ^
+    -DENABLE_SYSTEM_OPENCL=ON                                                       ^
+    -DENABLE_SYSTEM_PROTOBUF=ON                                                     ^
+    -DENABLE_SYSTEM_SNAPPY=ON                                                       ^
+    -DOPENVINO_EXTRA_MODULES="%SRC_DIR%/openvino_contrib/modules/nvidia_plugin"     ^
+    -DBUILD_nvidia_plugin=ON                                                        ^
+    -DENABLE_COMPILE_TOOL=OFF                                                       ^
+    -DENABLE_PYTHON=OFF                                                             ^
+    -DENABLE_CPPLINT=OFF                                                            ^
+    -DENABLE_CLANG_FORMAT=OFF                                                       ^
+    -DENABLE_NCC_STYLE=OFF                                                          ^
+    -DENABLE_TEMPLATE=OFF                                                           ^
+    -DENABLE_SAMPLES=OFF                                                            ^
+    -DENABLE_DATA=OFF                                                               ^
+    -DCPACK_GENERATOR=CONDA-FORGE                                                   ^
+    -G Ninja                                                                        ^
+    -S "%SRC_DIR%"                                                                  ^
     -B "%SRC_DIR%/build"
 if errorlevel 1 exit 1
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -8,27 +8,26 @@ fi
 
 mkdir -p build
 
-cmake ${CMAKE_ARGS}                                                          \
-    -DCMAKE_BUILD_TYPE=Release                                               \
-    -DENABLE_INTEL_GNA=OFF                                                   \
-    -DENABLE_SYSTEM_TBB=ON                                                   \
-    -DENABLE_SYSTEM_PUGIXML=ON                                               \
-    -DENABLE_SYSTEM_PROTOBUF=ON                                              \
-    -DENABLE_SYSTEM_SNAPPY=ON                                                \
-    -DOPENVINO_EXTRA_MODULES="$SRC_DIR/openvino_contrib/modules"             \
-    -DBUILD_nvidia_plugin=ON                                                 \
-    -DBUILD_java_api=OFF                                                     \
-    -DENABLE_COMPILE_TOOL=OFF                                                \
-    -DENABLE_PYTHON=OFF                                                      \
-    -DENABLE_CPPLINT=OFF                                                     \
-    -DENABLE_CLANG_FORMAT=OFF                                                \
-    -DENABLE_NCC_STYLE=OFF                                                   \
-    -DENABLE_TEMPLATE=OFF                                                    \
-    -DENABLE_SAMPLES=OFF                                                     \
-    -DENABLE_DATA=OFF                                                        \
-    -DCPACK_GENERATOR=CONDA-FORGE                                            \
-    -G Ninja                                                                 \
-    -S "$SRC_DIR"                                                            \
+cmake ${CMAKE_ARGS}                                                                         \
+    -DCMAKE_BUILD_TYPE=Release                                                              \
+    -DENABLE_INTEL_GNA=OFF                                                                  \
+    -DENABLE_SYSTEM_TBB=ON                                                                  \
+    -DENABLE_SYSTEM_PUGIXML=ON                                                              \
+    -DENABLE_SYSTEM_PROTOBUF=ON                                                             \
+    -DENABLE_SYSTEM_SNAPPY=ON                                                               \
+    -DOPENVINO_EXTRA_MODULES="$SRC_DIR/openvino_contrib/modules/nvidia_plugin"              \
+    -DBUILD_nvidia_plugin=ON                                                                \
+    -DENABLE_COMPILE_TOOL=OFF                                                               \
+    -DENABLE_PYTHON=OFF                                                                     \
+    -DENABLE_CPPLINT=OFF                                                                    \
+    -DENABLE_CLANG_FORMAT=OFF                                                               \
+    -DENABLE_NCC_STYLE=OFF                                                                  \
+    -DENABLE_TEMPLATE=OFF                                                                   \
+    -DENABLE_SAMPLES=OFF                                                                    \
+    -DENABLE_DATA=OFF                                                                       \
+    -DCPACK_GENERATOR=CONDA-FORGE                                                           \
+    -G Ninja                                                                                \
+    -S "$SRC_DIR"                                                                           \
     -B "$SRC_DIR/build"
 
 cmake --build "$SRC_DIR/build" --config Release --parallel $CPU_COUNT --verbose
