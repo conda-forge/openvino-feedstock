3f1e3d084405036f994687d02b06ed623afe0a5a
Anokhov <artyom.anokhov@intel.com>
2023-07-20 22:42:16 +0200

    Updated copy for licenses

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -29,10 +29,8 @@ if errorlevel 1 exit 1
 cmake --build "%SRC_DIR%/build" --config Release --verbose
 if errorlevel 1 exit 1
 
-cp "%SRC_DIR%/LICENSE" LICENSE
-cp "%SRC_DIR%/licensing/third-party-programs.txt" third-party-programs.txt
-cp "%SRC_DIR%/licensing/onednn_third-party-programs.txt" onednn_third-party-programs.txt
-cp "%SRC_DIR%/licensing/runtime-third-party-programs.txt" runtime-third-party-programs.txt
-cp "%SRC_DIR%/licensing/tbb_third-party-programs.txt" tbb_third-party-programs.txt
+COPY "%SRC_DIR%\licensing\third-party-programs.txt" third-party-programs.txt
+COPY "%SRC_DIR%\licensing\onednn_third-party-programs.txt" onednn_third-party-programs.txt
+COPY "%SRC_DIR%\licensing\runtime-third-party-programs.txt" runtime-third-party-programs.txt
 
 exit 0
