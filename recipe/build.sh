3b4806e0f796ca5ef9fdb529f96048ec6501c7fc
Ilya Lavrenov <ilya.lavrenov@intel.com>
2023-07-26 11:20:14 +0400

    Disable deprecation warnings

--- recipe/build.sh
+++ recipe/build.sh
@@ -22,6 +22,8 @@ cmake ${CMAKE_ARGS}                                                          \
     -DENABLE_TEMPLATE=OFF                                                    \
     -DENABLE_SAMPLES=OFF                                                     \
     -DENABLE_DATA=OFF                                                        \
+    -DCMAKE_CXX_FLAGS="-Wno-deprecated-declarations"                         \
+    -DCMAKE_C_FLAGS="-Wno-deprecated-declarations"                           \
     -DCPACK_GENERATOR=CONDA-FORGE                                            \
     -G Ninja                                                                 \
     -S "$SRC_DIR"                                                            \
