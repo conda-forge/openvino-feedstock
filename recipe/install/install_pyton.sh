6d0012fdb4cd7c591a839379f8abbccabb396bfe
Anokhov <artyom.anokhov@intel.com>
2023-08-31 14:28:48 +0200

    Manually merge changes from 'dev'-branch

---
+++ recipe/install/install_pyton.sh
@@ -0,0 +1,10 @@
+# !/usr/bin/env bash
+
+export OPENVINO_BINARY_DIR="$SRC_DIR/build"
+export PY_PACKAGES_DIR="lib/python$PY_VER/site-packages"
+export WHEEL_VERSION="$PKG_VERSION"
+export PYTHON_EXTENSIONS_ONLY="1"
+export SKIP_RPATH="1"
+export CPACK_GENERATOR="CONDA-FORGE"
+
+$PYTHON -m pip install --no-deps --ignore-installed -v "$SRC_DIR/src/bindings/python/wheel"
