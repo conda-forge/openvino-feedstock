2db31ffc65f25ef2a9309210c2eb07e7c1de3772
Ilya Lavrenov <ilya.lavrenov@intel.com>
2024-05-25 18:06:16 +0400

    Pre-release 2024.2.0

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,27 +1,24 @@
 echo ON
 setlocal enabledelayedexpansion
 
-rem use rapidjson from conda-forge dependencies
-RMDIR /S /Q "$SRC_DIR/src/plugins/intel_gpu/thirdparty/rapidjson"
-
 mkdir -p build
 
 cmake                                                                        ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"                                ^
     -DCMAKE_BUILD_TYPE=Release                                               ^
-    -DENABLE_INTEL_GNA=OFF                                                   ^
     -DENABLE_SYSTEM_TBB=ON                                                   ^
     -DENABLE_SYSTEM_PUGIXML=ON                                               ^
     -DENABLE_SYSTEM_OPENCL=ON                                                ^
     -DENABLE_SYSTEM_PROTOBUF=ON                                              ^
     -DENABLE_SYSTEM_SNAPPY=ON                                                ^
+    -DENABLE_JS=OFF                                                          ^
     -DENABLE_PYTHON=OFF                                                      ^
     -DENABLE_CPPLINT=OFF                                                     ^
     -DENABLE_CLANG_FORMAT=OFF                                                ^
     -DENABLE_NCC_STYLE=OFF                                                   ^
+    -DENABLE_INTEL_NPU=OFF                                                   ^
     -DENABLE_TEMPLATE=OFF                                                    ^
     -DENABLE_SAMPLES=OFF                                                     ^
-    -DENABLE_DATA=OFF                                                        ^
     -DCMAKE_CXX_FLAGS="/wd4996"                                              ^
     -DCMAKE_C_FLAGS="/wd4996"                                                ^
     -DCPACK_GENERATOR=CONDA-FORGE                                            ^
@@ -30,7 +27,7 @@ cmake                                                                        ^
     -B "%SRC_DIR%\build"
 if errorlevel 1 exit 1
 
-cmake --build "%SRC_DIR%\build" --config Release --parallel %CPU_COUNT% --verbose
+cmake --build "%SRC_DIR%\build" --config Release --parallel %CPU_COUNT%
 if errorlevel 1 exit 1
 
 COPY "%SRC_DIR%\licensing\third-party-programs.txt" third-party-programs.txt
