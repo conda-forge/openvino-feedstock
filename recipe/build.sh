7a31f39e28e254e0981df98edd20282b0e839183
Ilya Lavrenov <ilya.lavrenov@intel.com>
2024-12-31 11:05:56 +0100

    Updates

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,10 +12,11 @@ cmake ${CMAKE_ARGS}                                                          \
     -DCMAKE_BUILD_TYPE=Release                                               \
     -DENABLE_SYSTEM_TBB=ON                                                   \
     -DENABLE_SYSTEM_PUGIXML=ON                                               \
-    -DENABLE_INTEL_NPU_INTERNAL=OFF                                          \
-    -DENABLE_OV_JAX_FRONTEND=OFF                                             \
     -DENABLE_SYSTEM_PROTOBUF=ON                                              \
     -DENABLE_SYSTEM_SNAPPY=ON                                                \
+    -DENABLE_SYSTEM_LEVEL_ZERO=ON                                            \
+    -DENABLE_INTEL_NPU_INTERNAL=OFF                                          \
+    -DENABLE_OV_JAX_FRONTEND=OFF                                             \
     -DENABLE_JS=OFF                                                          \
     -DENABLE_PYTHON=OFF                                                      \
     -DENABLE_CPPLINT=OFF                                                     \
