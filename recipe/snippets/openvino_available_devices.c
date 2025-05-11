d6ff24f1e067a080f102e286f562f4569a316fce
Mark Harfouche <mark.harfouche@gmail.com>
2025-05-10 20:00:16 -0400

    Add some tests

--- recipe/snippets/openvino_available_devices.c
+++ recipe/snippets/openvino_available_devices.c
@@ -10,23 +10,31 @@
 int main() {
     ov_core_t* core = NULL;
     char* ret = NULL;
+    printf("OpenVINO version: %s\n", ov_get_openvino_version());
     OV_CALL(ov_core_create(&core));
+    printf("OpenVINO core created\n");
     OV_CALL(ov_core_get_property(core, "CPU", "AVAILABLE_DEVICES", &ret));
+    printf("CPU available devices: %s\n", ret);
 #if defined(OPENVINO_ARCH_X86_64) && !defined(__APPLE__)
     OV_CALL(ov_core_get_property(core, "GPU", "AVAILABLE_DEVICES", &ret));
+    printf("GPU available devices: %s\n", ret);
     // OV_CALL(ov_core_get_property(core, "NPU", "AVAILABLE_DEVICES", &ret));
 #endif
 #ifdef CONDA_FORGE_OPENVINO_AUTO_FRONTEND
     OV_CALL(ov_core_get_property(core, "AUTO", "SUPPORTED_PROPERTIES", &ret));
+    printf("AUTO supported properties: %s\n", ret);
 #endif
 #ifdef CONDA_FORGE_OPENVINO_MULTI_BACKEND
     OV_CALL(ov_core_get_property(core, "MULTI", "SUPPORTED_PROPERTIES", &ret));
+    printf("MULTI supported properties: %s\n", ret);
 #endif
 #ifdef CONDA_FORGE_OPENVINO_HETERO_BACKEND
     OV_CALL(ov_core_get_property(core, "HETERO", "SUPPORTED_PROPERTIES", &ret));
+    printf("HETERO supported properties: %s\n", ret);
 #endif
 #ifdef CONDA_FORGE_OPENVINO_BATCH_BACKEND
     OV_CALL(ov_core_get_property(core, "BATCH", "SUPPORTED_PROPERTIES", &ret));
+    printf("BATCH supported properties: %s\n", ret);
 #endif
     ov_core_free(core);
     return EXIT_SUCCESS;
